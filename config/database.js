17c501f2457c83ed7dab2fea6d8e3e6c9e751e31
jopyhenriques <jopy.henriques@gmail.com>
2016-03-08 15:48:29 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-	url : 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.MONGODB_DATABASE
+	url : 'mongodb://jopy:jopy@mongodb:27017/todo'
 }
