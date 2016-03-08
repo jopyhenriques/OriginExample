69336c0987d5eb8004093a29d137b2297590a2d1
jopyhenriques <jopy.henriques@gmail.com>
2016-03-08 09:46:12 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-	url : 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.MONGODB_DATABASE
+	url : 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.MONGODB_SERVICE_HOST + ':' + MONGODB_SERVICE_PORT + '/' + process.env.MONGODB_DATABASE
 }
