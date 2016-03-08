f518ddc8fb6e8baa5c37a94e7f5b177a05b14131
jopyhenriques <jopy.henriques@gmail.com>
2016-03-08 10:10:54 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-	url : 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.MONGODB_SERVICE_HOST + ':' + MONGODB_SERVICE_PORT + '/' + process.env.MONGODB_DATABASE
+	url : 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT + '/' + process.env.MONGODB_DATABASE
 }
